20410b28fa6e47702cc2b514518c93163d253f65
cis101 <cis101@secs.iub.edu.bd>
2019-05-29 09:33:07 +0600

    sorting

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 function sum1(arr) {
 
     var sum = 0;
-    arr.forEach(function (item, index){
+    arr.forEach(function (item, index) {
         sum = sum + item;
 
-       
-    }) 
+
+    })
     return sum;
 }
 console.log(sum1([1, 1, 1, 1, 1]));
 
 //Task 3: Email validation with regex
 
-function validateEmail(email){
-   return  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+function validateEmail(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 }
 
 
@@ -25,16 +25,15 @@ console.log(validateEmail("hello"))
 //Task 4: Status printing
 
 var company = {
-    name: "Google", 
-    age: 19, 
+    name: "Google",
+    age: 19,
     ceo: {
-    first_name: "Sundar", 
-    last_name: "Pichai"
+        first_name: "Sundar",
+        last_name: "Pichai"
     }
-    };
-    console.log(company);
-company.updateAge = function(newAge)
-{this.age= newAge; }
+};
+console.log(company);
+company.updateAge = function (newAge) { this.age = newAge; }
 company.updateAge(23)
 
 console.log(company);
@@ -52,14 +51,67 @@ var library = [
     },
     {
         author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        title: 'Mockingjay: The Final Book of The Hunger Games',
         readingStatus: false
     }];
 
 
-function getProp(index,prop){
+function getProp(index, prop) {
     return library[index][prop]
 
 }
-console.log(getProp(1,'author'))
+console.log(getProp(1, 'author'))
 
+// Task 5
+var cart = [
+    {
+        name: 'Shoes',
+        price: 560,
+        quantity: 4
+    },
+    {
+        name: 'Regular Tees',
+        price: 455.50,
+        quantity: 6
+    },
+    {
+        name: 'Socks',
+        price: 65.99,
+        quantity: 2
+    }];
+//Task 5a= add a new item
+function addItem(newItem) {
+    cart.push(newItem)
+}
+
+addItem( {name: 'hello',
+price: 100,
+quantity: 2})
+
+
+//Task 5 b-SOrt by a given property
+function sortCart(prop) {
+    cart.sort(function(a,b){
+       if( a[prop]<b[prop]){
+        return-1;}
+        if ( a[prop]>b[prop]){
+            return 1;}
+        return 0;
+    });
+
+    
+}
+sortCart('price');
+
+console.log(cart);
+// Task 5c - find item by name 
+function findByName() {
+
+}
+//task 5d- get total cost
+
+function getTotalcost() {
+
+
+
+}
