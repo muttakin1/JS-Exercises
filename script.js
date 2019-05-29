4de1845f9116de3062536ba2dd42b4603131d03e
cis101 <cis101@secs.iub.edu.bd>
2019-05-29 08:46:04 +0600

    task 4

--- script.js
+++ script.js
@@ -19,4 +19,47 @@ function validateEmail(email){
    return  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 }
 
+
 console.log(validateEmail("hello"))
+
+//Task 4: Status printing
+
+var company = {
+    name: "Google", 
+    age: 19, 
+    ceo: {
+    first_name: "Sundar", 
+    last_name: "Pichai"
+    }
+    };
+    console.log(company);
+company.updateAge = function(newAge)
+{this.age= newAge; }
+company.updateAge(23)
+
+console.log(company);
+
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+
+
+function getProp(index,prop){
+    return library[index][prop]
+
+}
+console.log(getProp(1,'author'))
+
